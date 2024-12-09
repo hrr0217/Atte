4973d8d83a6c6496c0dbf8a88ce7c2fb2f10f53f
haruna <neotokyo147@gmail.com>
2024-12-09 15:52:00 +0900

    new

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AtteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AtteController::class, 'index']);
+
+Route::get('/login', [AtteController::class, 'login']);
